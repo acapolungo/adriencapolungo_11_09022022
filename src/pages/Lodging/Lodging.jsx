ee3438601e5da4b8dcc9541f98c695bfb04001cc
Adrien Capolungo <adrien.capolungo@gmail.com>
2022-02-11 18:51:48 +0100

    bug loading async

--- src/pages/Lodging/Lodging.jsx
+++ src/pages/Lodging/Lodging.jsx
@@ -12,42 +12,46 @@ import { getLodgingById } from '../../query';
 
 export default function Lodging() {
 
-  const [lodging, setLodging] = useState({})
+  const [lodging, setLodging] = useState(null)
   const {lodgingId} = useParams()
+  let html;
 
+  // debugger
   useEffect(() => {
     getLodgingById(lodgingId).then(data => setLodging(data))
     .catch(err => console.log(err))
-  }, [lodgingId])
-
-  console.log(lodging)
+  })
 
   // const {id, pictures, title, description, equipments, host, location, rating, tags} = setLodging(lodgingId)
-  
-  return (
-    <main className='main'>
-    <Gallery pictures={lodging.pictures} key={lodging.id} />
-    <section className="lodgingdetail">
-        <div className="lodgingdetail__left">
-            <h1 className="lodgingdetail__title">{lodging.title}</h1>
-            <p className="lodgingdetail__location">{lodging.location}</p>
-            <Tags tags={lodging.tags} />
-        </div>
-          <div className="lodgingdetail__right">
-            <div className="lodgingdetail__host">
-                <div className="lodgingdetail__info">
-                    <p className="lodgingdetail__firstname">{lodging.host.name.split(' ', 1)}</p>
-                    <p className="lodgingdetail__lastname">{lodging.host.name.split(' ').pop()}</p>
-                </div>
-                <img src={lodging.host.picture} alt="Hôte" />
-            </div>
-            <Ratings ratings={lodging.rating} id={lodging.id} />
-        </div> 
-      </section>
-      <section className="general-info">
-      </section>
-    </main>
-  )
+  if (lodging) {
+    html = (
+      <main className='main'>
+      <Gallery pictures={lodging.pictures} key={lodging.id} />
+      <section className="lodgingdetail">
+          <div className="lodgingdetail__left">
+              <h1 className="lodgingdetail__title">{lodging.title}</h1>
+              <p className="lodgingdetail__location">{lodging.location}</p>
+              <Tags tags={lodging.tags} />
+          </div>
+            <div className="lodgingdetail__right">
+              <div className="lodgingdetail__host">
+                  <div className="lodgingdetail__info">
+                      <p className="lodgingdetail__firstname">{lodging.host.name.split(' ', 1)}</p>
+                      <p className="lodgingdetail__lastname">{lodging.host.name.split(' ').pop()}</p>
+                  </div>
+                  <img src={lodging.host.picture} alt="Hôte" />
+              </div>
+              <Ratings ratings={lodging.rating} id={lodging.id} />
+          </div> 
+        </section>
+        <section className="general-info">
+        </section>
+      </main>
+    )
+  } else {
+    html = 'toto'
+  }
+  return html
 }
 
 // export default function Lodging({getLodging}) {
